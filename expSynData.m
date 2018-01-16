2870e014e6694e9801b8e8cfe6098ba769c8c69c
Yue Niu <yue.niu@foxmail.com>
2018-01-16 22:11:23 +0800

    Modify: Optimize run control in which certain algorithm can be control to run or not.

--- expSynData.m
+++ expSynData.m
@@ -5,8 +5,8 @@ clear all
 expTimes = 1;
 dataSize = 900;
 noiseLevel = 0.0;
-bandNum = 4;
-emNum = 4;
+bandNum = 5;
+emNum = 6;
 HTrue = zeros(emNum, bandNum);
 HI = zeros(emNum, bandNum);
 HMdc = zeros(emNum, bandNum);
@@ -22,11 +22,17 @@ xlabel('band 1');
 ylabel('band 2');
 
 % Run VCA
-HVca = hyperVca(V', emNum);
+vca_ = false;
+if vca_
+    HVca = hyperVca(V', emNum);
+end
 
 % Run Nfindr
 indxNfindr = nFindr(V, emNum);
 HNfindr = V(indxNfindr, :);
+sadNfindr = sadEms(HTrue, HNfindr, emNum);
+sadSet = sadNfindr;
+legendBar = { 'N-Findr' };
 
 % Run nmfAbundance to find initial well-conditioned abundance.
 HI = HNfindr;
@@ -37,92 +43,137 @@ maxIter = 5000;
                         alpha, tol, maxIter);
 
 % Run hyperNmfMDC.
-[ WMdc, HMdc, HRcMDC, EMDC] = ...
-    hyperNmfMDC(...
-    V, emNum, WI, HI, ...
-    0.001, 4,...
-    0.01, 30000 );
+mdc_ = true;
+if mdc_
+    [ WMdc, HMdc, HRcMDC, EMDC] = ...
+        hyperNmfMDC(...
+        V, emNum, WI, HI, ...
+        0.0002, 4,...
+        0.01, 30000 );
+    sadMdc = sadEms(HTrue, HMdc, emNum);
+    sadSet = [sadSet, sadMdc];
+    legendBar{ length(legendBar)+1 } = 'MDC';
+end
 
 % Run hyperNmfMVC
-[UU, SS, WW] = svd(V');
-prinComp = pca(V);
-meanData = mean(HTrue, 1);
+mvc_ = true;
+if mvc_
+    [UU, SS, WW] = svd(V');
+    prinComp = pca(V);
+    meanData = mean(HTrue, 1);
 
-[HMvc, WMvc] = hyperNmfMVC(V', HI', WI', ...
-    HI', UU, prinComp, meanData, ...
-    0.015, 0.001, 30000, ...
-    0, 2, 1);
+    [HMvc, WMvc] = hyperNmfMVC(V', HI', WI', ...
+        HI', UU, prinComp, meanData, ...
+        0.015, 0.001, 100, ...
+        0, 2, 1);
+    sadMvc = sadEms(HTrue, HMvc', emNum);
+    sadSet = [sadSet, sadMvc];
+    legendBar{ length(legendBar)+1 } = 'ICE';
+end
 
 % Run hyperNmfASCL1_2
-[ WASCL1_2, HASCL1_2, HRcL1_2, errRcL1_2, objRcL1] = ...
-    hyperNmfASCL1_2(...
-    V', HI', WI',...
-    0.001,... % tolObj
-    20000,... % maxIter
-    20 ... %fDelta
-    );
+spl1_2_ = false;
+if spl1_2_
+    [ WASCL1_2, HASCL1_2, HRcL1_2, errRcL1_2, objRcL1] = ...
+        hyperNmfASCL1_2(...
+        V', HI', WI',...
+        0.001,... % tolObj
+        20000,... % maxIter
+        20 ... %fDelta
+        );
+    sadASCL1_2 = sadEms(HTrue, HASCL1_2, emNum);
+    sadSet = [sadSet, sadASCL1_2];
+    legendBar{ length(legendBar) } = 'ASCL_{1/2}';
+end
 
 % Run hyperNmfASCL1
-[ WASCL1, HASCL1, HRcL1, errRcL1, objRcL1] = ...
-    hyperNmfASCL1(...
-    V', HI', WI',...
-    0.001,... % tolObj
-    20000,... % maxIter
-    20 ... %fDelta
-    );
-
+spl1_ = false;
+if spl1_
+    [ WASCL1, HASCL1, HRcL1, errRcL1, objRcL1] = ...
+        hyperNmfASCL1(...
+        V', HI', WI',...
+        0.001,... % tolObj
+        20000,... % maxIter
+        20 ... %fDelta
+        );
+    sadASCL1 = sadEms(HASCL1, HASCL1_2, emNum);
+    sadSet = [sadSet, sadASCL1];
+    legendBar{ length(legendBar) } = 'ASCL_1';
+end
 
 % Visualize result
 figure;
 hold on
 % true data
-scatter(V(:,1), V(:,2));
-hTrue = scatter(HTrue(:, 1), HTrue(:, 2), 'filled', 'r');
+scatter(V(:,1), V(:,2), 'm+');
+hTrue = scatter(HTrue(:, 1), HTrue(:, 2), 100, 'filled', 'r');
+handlesScatter = hTrue;
+legendScatter = {'True'};
+
 % VCA
-hVCA = scatter(HVca(1,:), HVca(2,:), 100, 'filled', 'o');
-% Nfindr
-hNfindr = scatter(HNfindr(:, 1), HNfindr(:, 2), 'filled', 'm');
-% MDC
-bandIndx1 = 1;
-bandIndx2 = 2;
-hMDC = scatter( HMdc(:,bandIndx1), HMdc(:,bandIndx2) , 'filled','k');
-for i=1:bandNum
-    plot(HRcMDC(i, :,bandIndx1), HRcMDC(i, :,bandIndx2), 'm-', 'MarkerSize', 5);
-end
-% MVC
-hMVC = scatter( HMvc(bandIndx1,:), HMvc(bandIndx2,:) , 'filled','b');
-% ASCL1_2
-hASCL1_2 = scatter( HASCL1_2(bandIndx1, :), HASCL1_2(bandIndx2,:) , 'filled','c');
-for i = 1:emNum
-    plot(HRcL1_2(i, :,bandIndx1), HRcL1_2(i, :,bandIndx2), 'r-.', 'MarkerSize', 5);
-end
-% ASCL1
-hASCL1 = scatter( HASCL1(bandIndx1, :), HASCL1(bandIndx2,:) , 'filled','g');
-for i = 1:emNum
-    plot(HRcL1(i, :,bandIndx1), HRcL1(i, :,bandIndx2), 'r-.', 'MarkerSize', 5);
+if vca_
+    hVCA = scatter(HVca(1,:), HVca(2,:), 100, 'filled', 'o');
+    handlesScatter = [handlesScatter, hVCA];
+    legendScatter{ length(legendScatter)+1 } = 'VCA';
 end
 
-legend([hTrue,hVCA,hNfindr,hMDC,hMVC,hASCL1_2,hASCL1], 'True', 'VCA', 'Nfindr', 'MDC', 'MVC', 'ASCL1/2', 'ASCL1')
+% Nfindr
+hNfindr = scatter(HNfindr(:, 1), HNfindr(:, 2), 100, 'filled', 'm');
+handlesScatter = [handlesScatter, hNfindr];
+legendScatter{ length(legendScatter)+1 } = 'N-Findr';
+
+% MDC
+if mdc_
+    bandIndx1 = 1;
+    bandIndx2 = 2;
+    hMDC = scatter( HMdc(:,bandIndx1), HMdc(:,bandIndx2) , 100, 'filled','k');
+    for i=1:bandNum
+        plot(HRcMDC(i, :,bandIndx1), HRcMDC(i, :,bandIndx2), 'm-', 'MarkerSize', 5);
+    end
+    VMdc = WMdc * HMdc;
+    scatter(VMdc(:,1), VMdc(:,2), 'o');
+    handlesScatter = [handlesScatter, hMDC];
+    legendScatter{ length(legendScatter)+1 } = 'MDC';
+end
+
+% MVC
+if mvc_
+    hMVC = scatter( HMvc(bandIndx1,:), HMvc(bandIndx2,:) , 100, 'filled','b');
+    handlesScatter = [handlesScatter, hMVC];
+    legendScatter{ length(legendScatter)+1 } = 'ICE';
+end
+
+% ASCL1_2
+if spl1_2_
+    hASCL1_2 = scatter( HASCL1_2(bandIndx1, :), HASCL1_2(bandIndx2,:) , 100, 'filled','c');
+    for i = 1:emNum
+        plot(HRcL1_2(i, :,bandIndx1), HRcL1_2(i, :,bandIndx2), 'r-.', 'MarkerSize', 5);
+    end
+    handlesScatter = [handlesScatter, hASCL1_2];
+    legendScatter{ length(legendScatter)+1 } = 'ASCL_{1/2}';
+end
+
+% ASCL1
+if spl1_
+    hASCL1 = scatter( HASCL1(bandIndx1, :), HASCL1(bandIndx2,:) , 100, 'filled','g');
+    for i = 1:emNum
+        plot(HRcL1(i, :,bandIndx1), HRcL1(i, :,bandIndx2), 'r-.', 'MarkerSize', 5);
+    end
+    handlesScatter = [handlesScatter, hASCL1];
+    legendScatter{ length(legendScatter)+1 } = 'ASCL_1';
+end
+
+legend(handlesScatter, legendScatter)
 
 
 % Computer reconstruction error.
 alpha = 1;
 tol = 0.1;
 maxIter = 5000;
-WVca = nmfAbundance(V, emNum, HVca',...
-                        alpha, tol, maxIter);
-errVca = sum(sum((V-WVca*HVca').^2));
-errNfindr = sum(sum((V-WI*HNfindr).^2));
-errMdc = sum(sum((V-WMdc*HMdc).^2));
-errMvc = sum(sum((V-WMvc'*HMvc').^2));
-errASCL1_2 = sum(sum((V-WASCL1_2'*HASCL1_2(1:emNum,:)').^2));
-errASCL1 = sum(sum((V-WASCL1'*HASCL1(1:emNum,:)').^2));
 figure;
 hold on
-heVCA = plot(1, errVca, '*', 'MarkerSize', 10, 'Color', 'r');
-heNfindr = plot(1, errNfindr, '*', 'MarkerSize', 10, 'Color', 'm');
-heMdc = plot(1, errMdc, '*', 'MarkerSize', 10, 'Color', 'k');
-heMvc = plot(1, errMvc, '*', 'MarkerSize', 10, 'Color', 'b');
-heASCL1_2 = plot(1, errASCL1_2, '*', 'MarkerSize', 10, 'Color', 'c');
-heASCL1 = plot(1, errASCL1, '*', 'MarkerSize', 10, 'Color', 'g'); 
-legend([heVCA,heNfindr,heMdc,heMvc,heASCL1_2,heASCL1], 'VCA', 'Nfindr', 'MDC', 'MVC', 'ASCL1/2', 'ASCL1');
+
+hBar = bar(sadSet);
+xlabel('endmember', 'FontSize', 15, 'FontWeight', 'bold')
+ylabel('SAD', 'FontSize', 15, 'FontWeight', 'bold')
+legend(hBar, legendBar)
